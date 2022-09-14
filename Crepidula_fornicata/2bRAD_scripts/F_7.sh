1d0e5300c1f28d05c2b6b29c8fa543a09abe2835
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2022-09-14 08:42:59 -0600

    exploring ANGSD filters

--- Crepidula_fornicata/2bRAD_scripts/F_7.sh
+++ Crepidula_fornicata/2bRAD_scripts/F_7.sh
@@ -2,13 +2,13 @@
 
 #$ -V # inherit the submission environment
 #$ -cwd #start job in submission directory
-#$ -N all_f1  # job name, anything you want
+#$ -N fornicata_f7  # job name, anything you want
 #$ -P davieslab
 #$ -l h_rt=12:00:00 #maximum run time
 #$ -M wuitchik@bu.edu #your email
 #$ -m be 
 
-FILTERS="-uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 30 -dosnpstat 1 -doHWE 1 -sb_pval 1e-5 -hetbias_pval 1e-5 -skipTriallelic 1 -minInd 130 -snp_pval 1e-5 -minMaf 0.05 -setMinDepthInd 1"
+FILTERS="-uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 30 -dosnpstat 1 -doHWE 1 -sb_pval 1e-5 -hetbias_pval 1e-5 -skipTriallelic 1 -minInd 69 -snp_pval 1e-5 -minMaf 0.05 -setMinDepthInd 7"
 TODO="-doMajorMinor 1 -doMaf 1 -doCounts 1 -makeMatrix 1 -doIBS 1 -doCov 1 -doGeno 8 -doBcf 1 -doPost 1 -doGlf 2"
 
-angsd -b filtered_bams -GL 1 $FILTERS $TODO -P 1 -out all_f1
+angsd -b fornicata_bams -GL 1 $FILTERS $TODO -P 1 -out fornicata_f7
